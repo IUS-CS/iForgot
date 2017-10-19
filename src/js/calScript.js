0fabb73b240e1063892117fc17b61b1e67c8563c
naveith <naveith@ius.edu>
2017-10-18 23:59:37 -0400

    Small weekday css changes

--- src/js/calScript.js
+++ src/js/calScript.js
@@ -10,24 +10,18 @@ window.onload = function(){
     var first_day = tmp.substring(0, 3);    //Sun
     var day_name = ['Sun','Mon','Tue','Wed','Thu','Fri','Sat'];
     var day_no = day_name.indexOf(first_day);   //1
-    var days = new Date(year, month + 1, 0).getDate();    //30
+    var days = new Date(year, month + 1, 0).getDate();    //31
     //Tue Oct 31 2014 ...
     var calendar = get_calendar(day_no, days);
-    document.getElementById("calendar-month-year").innerHTML = month_name[month]+" "+year;
+
+    document.getElementById("calendar-month").innerHTML = month_name[month];
+	document.getElementById("calendar-year").innerHTML = year;
     document.getElementById("calendar-dates").appendChild(calendar);
 }
 
 function get_calendar(day_no, days){
     var table = document.createElement('table');
     var tr = document.createElement('tr');
-    
-    //row for the day letters
-    for(var i = 0; i <= 6; i++){
-        var td = document.createElement('td');
-        td.innerHTML = "SMTWTFS"[i];
-        tr.appendChild(td);
-    }
-    table.appendChild(tr);
     
     //create 2nd row
     tr = document.createElement('tr');
@@ -41,7 +35,7 @@ function get_calendar(day_no, days){
     }
     
     var count = 1;
-    for(var k = 0; k <=6 ; k++){
+    for(var k = 0; k <= 6 ; k++){
         var td = document.createElement('td');
         td.innerHTML = count;
         count++;
@@ -50,9 +44,9 @@ function get_calendar(day_no, days){
     table.appendChild(tr);
     
     //rest of the date rows
-    for(var r=3; r<=7; r++){
+    for(var r = 3; r <= 7; r++){
         tr = document.createElement('tr');
-        for(var c=0; c<=6; c++){
+        for(var c = 0; c <= 6; c++){
             if(count > days){
                 table.appendChild(tr);
                 return table;
